e1843aa6f3d89a9d4e8241f889fea3bb897869b5
Ivan <ivan.sapaya@gmail.com>
2025-06-17 19:36:30 -0300

    fase2

---
+++ TemplateTPCorto/TemplateTPCorto/FormVentas.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace TemplateTPCorto
+{
+    partial class FormVentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListarProductos = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnQuitar = new System.Windows.Forms.Button();
+            this.lablSubTotal = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lstProducto = new System.Windows.Forms.ListBox();
+            this.cboCategoriaProductos = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbClientes = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnListarProductos
+            // 
+            this.btnListarProductos.Location = new System.Drawing.Point(296, 134);
+            this.btnListarProductos.Margin = new System.Windows.Forms.Padding(2);
+            this.btnListarProductos.Name = "btnListarProductos";
+            this.btnListarProductos.Size = new System.Drawing.Size(85, 19);
+            this.btnListarProductos.TabIndex = 33;
+            this.btnListarProductos.Text = "Listar productos";
+            this.btnListarProductos.UseVisualStyleBackColor = true;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(585, 443);
+            this.btnCargar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(76, 19);
+            this.btnCargar.TabIndex = 32;
+            this.btnCargar.Text = "Cargar Venta";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            // 
+            // btnQuitar
+            // 
+            this.btnQuitar.Location = new System.Drawing.Point(761, 183);
+            this.btnQuitar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnQuitar.Name = "btnQuitar";
+            this.btnQuitar.Size = new System.Drawing.Size(56, 19);
+            this.btnQuitar.TabIndex = 31;
+            this.btnQuitar.Text = "Quitar";
+            this.btnQuitar.UseVisualStyleBackColor = true;
+            // 
+            // lablSubTotal
+            // 
+            this.lablSubTotal.AutoSize = true;
+            this.lablSubTotal.Location = new System.Drawing.Point(547, 372);
+            this.lablSubTotal.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lablSubTotal.Name = "lablSubTotal";
+            this.lablSubTotal.Size = new System.Drawing.Size(13, 13);
+            this.lablSubTotal.TabIndex = 30;
+            this.lablSubTotal.Text = "0";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(194, 443);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(97, 19);
+            this.btnAgregar.TabIndex = 29;
+            this.btnAgregar.Text = "Agregar a carrito";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(126, 412);
+            this.txtCantidad.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(76, 20);
+            this.txtCantidad.TabIndex = 28;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(66, 412);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Cantidad";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(547, 401);
+            this.lblTotal.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(13, 13);
+            this.lblTotal.TabIndex = 26;
+            this.lblTotal.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(452, 401);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Total";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(452, 372);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "SubTotal";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(454, 92);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(290, 251);
+            this.listBox1.TabIndex = 23;
+            // 
+            // lstProducto
+            // 
+            this.lstProducto.FormattingEnabled = true;
+            this.lstProducto.Location = new System.Drawing.Point(63, 183);
+            this.lstProducto.Margin = new System.Windows.Forms.Padding(2);
+            this.lstProducto.Name = "lstProducto";
+            this.lstProducto.Size = new System.Drawing.Size(339, 212);
+            this.lstProducto.TabIndex = 22;
+            // 
+            // cboCategoriaProductos
+            // 
+            this.cboCategoriaProductos.FormattingEnabled = true;
+            this.cboCategoriaProductos.Location = new System.Drawing.Point(175, 134);
+            this.cboCategoriaProductos.Margin = new System.Windows.Forms.Padding(2);
+            this.cboCategoriaProductos.Name = "cboCategoriaProductos";
+            this.cboCategoriaProductos.Size = new System.Drawing.Size(92, 21);
+            this.cboCategoriaProductos.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 141);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Categoria Productos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 94);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Clientes";
+            // 
+            // cmbClientes
+            // 
+            this.cmbClientes.FormattingEnabled = true;
+            this.cmbClientes.Location = new System.Drawing.Point(175, 92);
+            this.cmbClientes.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbClientes.Name = "cmbClientes";
+            this.cmbClientes.Size = new System.Drawing.Size(160, 21);
+            this.cmbClientes.TabIndex = 18;
+            // 
+            // FormVentas
+            // 
+            this.ClientSize = new System.Drawing.Size(852, 524);
+            this.Controls.Add(this.btnListarProductos);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.btnQuitar);
+            this.Controls.Add(this.lablSubTotal);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lstProducto);
+            this.Controls.Add(this.cboCategoriaProductos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbClientes);
+            this.Name = "FormVentas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListarProductos;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnQuitar;
+        private System.Windows.Forms.Label lablSubTotal;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lstProducto;
+        private System.Windows.Forms.ComboBox cboCategoriaProductos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbClientes;
+    }
+}
