a9a529e87e56af171cbb10b8d866ddc7315b7392
Ivan <ivan.sapaya@gmail.com>
2025-06-20 15:20:22 -0300

    agregar a carrito actualizar totales

--- TemplateTPCorto/TemplateTPCorto/FormVentas.cs
+++ TemplateTPCorto/TemplateTPCorto/FormVentas.cs
@@ -2,6 +2,7 @@
 using Datos.Ventas;
 using Negocio;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,14 +16,19 @@ namespace TemplateTPCorto
 {
     public partial class FormVentas : Form
     {
+        List<Producto> productos = new List<Producto>();
+        List<Producto> productosEnCarrito = new List<Producto>();
+        double subTotal = 0.0;
+        double total = 0.0;
+
         public FormVentas()
         {
             InitializeComponent();
+            this.Load += new System.EventHandler(this.FormVentas_Load);
         }
 
         private void FormVentas_Load(object sender, EventArgs e)
         {
-
             CargarClientes();
             CargarCategoriasProductos();
             IniciarTotales();
@@ -64,6 +70,85 @@ namespace TemplateTPCorto
             VentasNegocio ventasNegocio = new VentasNegocio();
 
 
+        }
+
+        private void cboCategoriaProductos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // borrar esto
+        }
+
+        private void btnListarProductos_Click_1(object sender, EventArgs e)
+        {
+            if (cboCategoriaProductos.Text.Equals(""))
+            {
+                MessageBox.Show("Debes seleccionar una categoria antes de listar los productos.");
+                return;
+            }
+            ProductoNegocio productoNegocio = new ProductoNegocio();
+            productos = productoNegocio.obtenerProductosPorCategoria(cboCategoriaProductos.Text);
+            lstProducto.Items.Clear();
+            foreach (Producto producto in productos)
+            {
+                lstProducto.Items.Add(producto);
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (lstProducto.Items.Count <= 0)
+            {
+                MessageBox.Show("Debes seleccionar una categoria y listar los productos.");
+                return;
+            }
+            int.TryParse(txtCantidad.Text, out int cantidadAComprar);
+            if (cantidadAComprar <= 0)
+            {
+                MessageBox.Show("La cantidad seleccionada debe ser mayor a 0.");
+                return;
+            }
+            String productoSeleccionadoString = lstProducto.GetItemText(lstProducto.SelectedItem);
+            if (productoSeleccionadoString.Equals(""))
+            {
+                MessageBox.Show("Debes seleccionar un producto para agregarlo al carrito.");
+                return;
+            }
+            Producto productoSeleccionado = getProductoPorNombre(productoSeleccionadoString);
+            if (productosEnCarrito.Exists(p => p.Nombre.Equals(productoSeleccionado.Nombre)))
+            {
+                MessageBox.Show("Ya has agregado este producto al carrito, si quieres cambiar la cantidad primero quitalo del carrito.");
+                return;
+            }
+            if (cantidadAComprar > productoSeleccionado.Stock)
+            {
+                MessageBox.Show("Cantidad seleccionada excede el stock disponible, por favor seleccione una cantidad valida.");
+                return;
+            }
+            productoSeleccionado.Stock = cantidadAComprar;
+            productosEnCarrito.Add(productoSeleccionado);
+            listBox1.Items.Add(productoSeleccionado);
+            subTotal += productoSeleccionado.Precio * cantidadAComprar;
+            total += productoSeleccionado.Precio * cantidadAComprar;
+            actualizarTotales();
+        }
+
+        private Producto getProductoPorNombre(String productoString)
+        {
+            String[] splitProducto = productoString.Split('|');
+            String nombre = splitProducto[0].Trim();
+            foreach (Producto producto in productos)
+            {
+                if (producto.Nombre.Equals(nombre))
+                {
+                    return producto;
+                }
+            }
+            return null;
+        }
+
+        private void actualizarTotales()
+        {
+            lablSubTotal.Text = subTotal.ToString();
+            lblTotal.Text = total.ToString();
         }
     }
 }
